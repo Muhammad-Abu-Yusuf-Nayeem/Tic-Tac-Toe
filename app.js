8adff2957e8a82019305c2313f7becd5cd2947cb
assignment-01 <ykhan9463@gmail.com>
2025-07-03 13:05:01 +0600

    added draw function

--- app.js
+++ app.js
@@ -23,13 +23,14 @@ const winPatterns = [
 const resetGame = () => {
   let turnO = true;
   enableBoxes();
+  moveCount = 0;
   msgContainer.classList.add("hide");
   location.reload();
 };
 
 boxes.forEach((box) => {
   box.addEventListener("click", () => {
-    if (box.innerText !== "") return; // prevent double click
+    if (box.innerText !== "") return;
 
     if (turnO) {
       box.innerText = "O";
@@ -42,6 +43,8 @@ boxes.forEach((box) => {
     }
 
     box.disabled = true;
+    moveCount++; // <--- add this
+
     checkWinner();
   });
 });
@@ -64,18 +67,29 @@ const showWinner = (winner) => {
   disableBoxes();
 };
 
+const showDraw = () => {
+  const msg = document.querySelector("#msg");
+  msg.innerText = "It's a Draw!";
+  document.querySelector(".msg-container").classList.remove("hide");
+};
+
 const checkWinner = () => {
   for (let pattern of winPatterns) {
     let pos1Val = boxes[pattern[0]].innerText;
     let pos2Val = boxes[pattern[1]].innerText;
     let pos3Val = boxes[pattern[2]].innerText;
-    // console.log(pattern);
-    if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
+
+    if (pos1Val !== "" && pos2Val !== "" && pos3Val !== "") {
       if (pos1Val === pos2Val && pos2Val === pos3Val) {
-        // console.log("winner", pos1Val);
         showWinner(pos1Val);
+        return;
       }
     }
+  }
+
+  // Check for draw
+  if (moveCount === 9) {
+    showDraw();
   }
 };
 
